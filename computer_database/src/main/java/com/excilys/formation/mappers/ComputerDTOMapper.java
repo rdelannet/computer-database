1a7b5abd1b18c04b5de8402f71114a4a6dee39f9
rdelannet <romain.delannet@hotmail.fr>
2020-06-25 17:57:06 +0200

    DTOComputer mapper

--- computer_database/src/main/java/com/excilys/formation/mappers/ComputerDTOMapper.java
+++ computer_database/src/main/java/com/excilys/formation/mappers/ComputerDTOMapper.java
@@ -1,5 +1,28 @@
 package com.excilys.formation.mappers;
 
-public class ComputerDTOMapper {
+import java.time.LocalDate;
 
+import com.excilys.formation.dto.CompanyDTO;
+import com.excilys.formation.dto.ComputerDTO;
+import com.excilys.formation.model.Computer;
+
+public class ComputerDTOMapper {
+	
+	public static Computer dtoToComputer (ComputerDTO computerDTO) {
+		return new Computer(
+			Integer.valueOf(computerDTO.getId()),
+			computerDTO.getName(),
+			LocalDate.parse(computerDTO.getIntroduced()),
+			LocalDate.parse(computerDTO.getDiscontinued()),
+			Integer.parseInt(computerDTO.getCompanyDTO().getId()));
+	}
+	
+	public static ComputerDTO computerToDTO (Computer computer) {
+		return new ComputerDTO(
+			Integer.valueOf(computer.getId()).toString(),
+			computer.getName(),
+			computer.getDateInt().toString(),
+			computer.getDateDisc().toString(),
+			//new CompanyDTO(Integer.valueOf(computer.getId()).toString(), computer.getComanyName()));
+	}			
 }
